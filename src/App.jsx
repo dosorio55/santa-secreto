fd1aef0bb9dac43a35a0cf716b90e7ed19ef3cea
Alejandro <diego.osorio@gorilast.com>
2022-12-05 12:42:02 +0100

    login form ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="flex justify-center align-middle w-screen min-h-screen">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
