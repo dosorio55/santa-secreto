9815e7680ef26fd315b29299a5ed5d82b8dcc54a
Alejandro <diego.osorio@gorilast.com>
2022-12-06 12:26:45 +0100

    login ready and adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // import logo from "./logo.svg";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="flex justify-center align-middle w-screen min-h-screen">
-      <Login />
+      <Routes>
+       {/*  <Route>
+          
+        </Route> */}
+        <Login />
+      </Routes>
     </div>
   );
 }
