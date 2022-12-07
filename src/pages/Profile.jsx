bdcd94365c9b9a0fab18931e503b213e909aced2
dosorio55 <dosorio55@gmail.com>
2022-12-07 09:54:04 +0100

    profile almost ready

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Spinner from "../components/Spinner";
 import { fetchData } from "../constants/settings";
 
 const Profile = ({ token }) => {
   const [userData, setUserData] = useState({});
+  const navigate = useNavigate();
+
   const getUser = async () => {
     try {
       const serverResponse = await fetchData(
@@ -21,19 +25,64 @@ const Profile = ({ token }) => {
       console.error(error);
     }
   };
+
   useEffect(() => {
+    if (!token) {
+      navigate("/");
+      return;
+    }
     getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleSanta = () => {};
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
-    <div className="items-center">
-      <img
-        src={userData.image}
-        className="w-20 h-20 rounded-full"
-        alt="profile"
-      />
-      <p>{userData.name}</p>
-      <button>Logout</button>
+    <div className="flex w-full min-h-screen justify-center items-center flex-col gap-10">
+      <div>
+        <div className="flex items-center gap-5">
+          <img
+            src={userData.image}
+            className="w-20 h-20 rounded-full"
+            alt="profile"
+          />
+          <p>{userData.name}</p>
+        </div>
+        <button
+          onClick={handleSanta}
+          className="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Mostrar mi santa secreto
+        </button>
+        <div className="flex flex-col gap-5">
+          <Spinner size={"medium"} />
+          <p>Estamos escogiendo tu SANTA SECRETO</p>
+        </div>
+        <div className="bg-slate-400 w-96 relative rounded-lg p-6">
+          <p className="absolute top-0 left-5">Mensaje de mi santa secreto</p>
+          <p>bla bla bla</p>
+        </div>
+        <button
+          onClick={handleLogout}
+          className="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          logout
+        </button>
+      </div>
+      <div className="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
+        <div className="absolute top-0 w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
+          <img
+            src="https://images.unsplash.com/photo-1609759843758-bf8cc831c13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="chrismas"
+            className="absolute inset-0 z-0 h-full w-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
+import addAvagar from "../assets/addAvatar.png";
 import { fetchData } from "../constants/settings";
 import { users } from "../settings/constants";
 
 const initialState = {
   name: "",
   password: "",
+  repeatPassword: "",
+  image: "",
 };
 
-const Login = ({ setToken }) => {
+const Register = ({ setToken }) => {
   const [formValue, setFormValue] = useState(initialState);
   const navigate = useNavigate();
+
+  const handleChangeImage = (event) => {
+    // setLoading(true);
+    const reader = new FileReader();
+
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = async () => {
+        const imageBase64 = reader.result;
+        console.log(imageBase64);
+        setFormValue((prevState) => ({ ...prevState, image: imageBase64 }));
+      };
+    }
+    // setLoading(false);
+  };
 
   const handleFormChange = (event) => {
     const { value, name } = event.target;
@@ -21,8 +39,7 @@ const Login = ({ setToken }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const serverResponse = await fetchData(formValue, null, "POST", "users/login");
-      console.log(serverResponse, formValue);
+      const serverResponse = await fetchData(formValue, null, "POST", "users");
       if (serverResponse.status === 200) {
         const data = await serverResponse.json();
         localStorage.setItem(
@@ -44,6 +61,27 @@ const Login = ({ setToken }) => {
         onSubmit={handleSubmit}
         className="md:w-1/2 sm:w-11/12 w-11/12 my-auto flex flex-col gap-6"
       >
+        <div className="w-full mb-8">
+          <label
+            htmlFor="add-image"
+            className="flex justify-center items-center gap-5 cursor-pointer"
+          >
+            <img
+              src={formValue.image || addAvagar}
+              className={`w-20 h-20 ${formValue.image && 'rounded-full'}`}
+              alt="profile"
+            />
+            Agregar foto
+          </label>
+          <input
+            type="file"
+            onChange={handleChangeImage}
+            name="add-image"
+            id="add-image"
+            className="hidden"
+            required
+          />
+        </div>
         <label
           htmlFor="users"
           className="blocktext-sm font-medium text-gray-900 dark:text-white"
@@ -84,16 +122,34 @@ const Login = ({ setToken }) => {
             Password
           </label>
         </div>
-        <div className="flex justify-center items-center gap-5 w-full">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            onChange={handleFormChange}
+            value={formValue.repeatPassword}
+            type="password"
+            name="repeatPassword"
+            id="floating_repeat_password"
+            placeholder=" "
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            required
+          />
+          <label
+            htmlFor="floating_repeat_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Confirm password
+          </label>
+        </div>
+        <div className="flex justify-center w-full items-center gap-5">
           <button
             type="submit"
             className="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            login
+            Registrar
           </button>
           <p>
             No tienes una cuenta,{" "}
-            <Link to={'/'} className="font-semibold ">REGÍSTRATE</Link>
+            <Link to={'/login'} className="font-semibold ">Login</Link>
           </p>
         </div>
       </form>
@@ -101,4 +157,4 @@ const Login = ({ setToken }) => {
   );
 };
 
-export default Login;
+export default Register;
